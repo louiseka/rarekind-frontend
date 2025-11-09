5992a16e4c79d92216dc4806f8823d3ab1c0ef44
Matt Nightingale <matt.snail@gmail.com>
2025-11-09 11:57:52 +0000

    Created and styled footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,15 @@ import React from "react";
 function Footer() {
   return (
     <div className={styles.wrapper}>
-          <h1>Footer</h1>
+      <div>
+          <button className={styles.footerLink}>Footer link 1</button>
+          <button className={styles.footerLink}>Footer link 2</button>
+          </div>
+          <div>
+          <button className={styles.footerSubLink}>Terms and Conditions</button>
+          <button className={styles.footerSubLink}>Cookies</button>
+          </div>
+          <p>© 2025 RareKind</p>
     </div>
   );
 }
