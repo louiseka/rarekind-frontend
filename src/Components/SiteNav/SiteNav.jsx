c50a6ad3511cba66d3dd1c6d1cd47721a85f149f
Matt Nightingale <matt.snail@gmail.com>
2025-11-10 08:46:02 +0000

    commented out unused navlinks for now

--- src/Components/SiteNav/SiteNav.jsx
+++ src/Components/SiteNav/SiteNav.jsx
@@ -8,11 +8,11 @@ function SiteNav() {
     const location = useLocation()
 
     const navLinks = [
-        { label: 'Dashboard', url: '/' },
-        { label: 'Users', url: '/users' },
+       // { label: 'Dashboard', url: '/' },  
         { label: 'All collections', url: '/allcollections' },
         { label: 'My collections', url: '/mycollections' },
-        { label: 'One of my collections', url: '/oneofmycollections' },
+         { label: 'Users', url: '/users' },
+       // { label: 'One of my collections', url: '/oneofmycollections' },
     ]
     return (
         <div className={styles.wrapper}>
