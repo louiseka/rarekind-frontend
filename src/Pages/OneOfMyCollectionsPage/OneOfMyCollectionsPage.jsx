5486a22bbb6bb8804fa2aa619745c63706cdbe65
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-11 12:08:51 +0000

    Renaming component

--- src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
+++ src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
@@ -2,7 +2,7 @@ import styles from './OneOfMyCollectionsPage.module.css'
 import CollectionDetails from '../../Components/CollectionDetails/CollectionDetails'
 import ItemCard from '../../Components/ItemCard/ItemCard'
 
-import Items from '../../Components/Items/Items'
+import AddItems from '../../Components/Items/AddItems'
 
 function OneOfMyCollectionsPage() {
     return (
@@ -12,7 +12,7 @@ function OneOfMyCollectionsPage() {
             </section>
 
             <section className={styles.itemContainer}>
-                {/* <Items /> */}
+                {/* <AddItems /> */}
                 <div className={styles.grid}>
                     <ItemCard />
                     <ItemCard />
