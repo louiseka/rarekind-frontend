99b0d90525a4b4e45ae802c9998049d75eb9a3d7
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-12 11:50:34 +0000

    Adding toggle button slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import popupReducer from '../Components/PopUp/popupSlice'
 import darkModeReducer from '../Components/DarkMode/darkModeSlice'
+import toggleReducer from '../Slices/toggleContentSlice'
 
 export const store = configureStore({
     reducer: {
         popup: popupReducer,
         darkmode: darkModeReducer,
+        toggle: toggleReducer,
     },
 })
