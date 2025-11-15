1b3ac9e43962a9aaa06a97f5a393c3d46c692066
Matt Nightingale <matt.snail@gmail.com>
2025-11-15 18:28:17 +0000

    added image url to item

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -4,6 +4,7 @@ export default function ItemCard({ item }) {
     return (
         <div className={styles.card}>
             <h3 className={styles.cardHeader}>{item.name}</h3>
+            <img src={item.image_url} alt={item.name} />
             <p>{item.description}</p>
         </div>
     )
