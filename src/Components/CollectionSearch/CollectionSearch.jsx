e149d197aa91c973b66e329fa620cac2d8bebc79
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-17 11:06:16 +0000

    Using context and updating button

--- src/Components/CollectionSearch/CollectionSearch.jsx
+++ src/Components/CollectionSearch/CollectionSearch.jsx
@@ -1,20 +1,24 @@
-import { FaSearch } from "react-icons/fa";
-import styles from "./CollectionSearch.module.css";
+import { FaSearch } from 'react-icons/fa'
+import { useSearch } from './SearchContext.jsx'
+import styles from './CollectionSearch.module.css'
 
 export default function CollectionSearch() {
-  return (
-    <div className={styles.searchContainer}>
-      <input
-        className={styles.searchInput}
-        aria-label="Search collections"
-        name="search"
-        id="search"
-        type="text"
-        placeholder="Search collections..."
-      ></input>
-      <button className={styles.searchButton} htmlFor="search">
-        <FaSearch />
-      </button>
-    </div>
-  );
+    const { query, setQuery } = useSearch()
+
+    return (
+        <div className={styles.searchContainer}>
+            <input
+                className={styles.searchInput}
+                aria-label="Search collections"
+                name="search"
+                id="search"
+                type="text"
+                placeholder="Search collections..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            ></input>
+
+            <FaSearch className={styles.searchButton} />
+        </div>
+    )
 }
