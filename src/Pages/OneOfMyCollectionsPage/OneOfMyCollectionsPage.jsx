059f727a316b5f31d4ab962c410015dd0735a818
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-18 12:09:40 +0000

    Updating logic to show collection name with toggle actions

--- src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
+++ src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
@@ -3,9 +3,8 @@ import CollectionDetails from '../../Components/CollectionDetails/CollectionDeta
 import ItemCard from '../../Components/ItemCard/ItemCard'
 import AddItems from '../../Components/AddItems/AddItems'
 import ToggleButton from '../../Components/ToggleButton/ToggleButton'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchItemsByCollectionId } from '../../Slices/itemAPISlice'
 import { fetchCollections } from '../../Slices/collectionAPISlice'
@@ -20,7 +19,7 @@ import {
 import Loading from '../../Components/Loading/Loading'
 
 function OneOfMyCollectionsPage() {
-    const selectedToggle = useSelector((state) => state.toggle.selectedToggle)
+    const [showContent, setShowContent] = useState(true)
     const dispatch = useDispatch()
     const items = useSelector((state) => state.items.items)
     const itemsStatus = useSelector((state) => state.items.status)
@@ -35,8 +34,6 @@ function OneOfMyCollectionsPage() {
     const classificationStatus = useSelector(
         (state) => state.classifications.status
     )
-
-    console.log(items)
 
     useEffect(() => {
         if (status === 'idle') {
@@ -67,20 +64,36 @@ function OneOfMyCollectionsPage() {
 
     return (
         <>
-            {!selectedToggle && (
-                <section className={styles.wrapper}>
-                    {status === 'loading' && <Loading />}
-                    {status === 'succeeded' && (
-                        <CollectionDetails
-                            collectionToShow={collectionToShow}
-                            classificationNameMap={classificationNameMap}
-                            items={items}
-                        />
-                    )}
-                </section>
-            )}
+            <section className={styles.wrapper}>
+                {collectionToShow ? (
+                    <>
+                        <h2 className={styles.pageTitle}>
+                            {collectionToShow.name}
+                        </h2>
+                        {showContent && (
+                            <>
+                                {status === 'loading' && <Loading />}
+                                {status === 'succeeded' && (
+                                    <CollectionDetails
+                                        collectionToShow={collectionToShow}
+                                        classificationNameMap={
+                                            classificationNameMap
+                                        }
+                                        items={items}
+                                    />
+                                )}
+                            </>
+                        )}
+                    </>
+                ) : (
+                    <Loading />
+                )}
+            </section>
 
-            <ToggleButton />
+            <ToggleButton
+                showContent={showContent}
+                handleToggle={() => setShowContent((prev) => !prev)}
+            />
             {itemsStatus === 'loading' && <Loading />}
             <section className={styles.itemContainer}>
                 {items.length <= 0 && <AddItems />}
