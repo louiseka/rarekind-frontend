83d07bbe635120bb2e38b34d2df7c65fd5c77db8
Matt Nightingale <matt.snail@gmail.com>
2025-11-19 12:24:22 +0000

    added suggested CSS to select dropdown

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,9 +1,29 @@
 import styles from './LoginForm.module.css'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector  } from 'react-redux'
 import { closePopup } from '../../Slices/popupSlice'
+import { useState } from 'react'
 
 function LoginForm() {
     const dispatch = useDispatch()
+    const { status, error } = useSelector((state) => state.auth)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addItem({ collectionId: id, updatedData: formData }))
+            .then(() => {
+                dispatch(fetchItemsByCollectionId(id))
+                dispatch(closePopup())
+            })
+            .catch((error) => console.error('Error logging in', error))
+    }
 
     return (
         <div className={styles.wrapper}>
@@ -23,6 +43,8 @@ function LoginForm() {
                         name="username"
                         placeholder="Enter email..."
                         className={styles.emailInput}
+                        onChange={handleChange}
+                        value={formData.email}
                     />
                 </label>
                 <label className={styles.label}>
@@ -32,10 +54,12 @@ function LoginForm() {
                         name="password"
                         placeholder="Enter password..."
                         className={styles.passwordInput}
+                        onChange={handleChange}
+                        value={formData.password}
                     />
                 </label>
                 <button type="submit" className={styles.button}>
-                    LOG IN
+                    {status === 'loading' ? 'LOGGING IN...' : 'LOG IN'}
                 </button>
             </form>
         </div>
