8fd9294b7a6ae529de99fcaed520776e228de743
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-19 14:45:49 +0000

    Making Add Item own component

---
+++ src/Components/AddItemButton/AddItemButton.jsx
@@ -0,0 +1,18 @@
+import { FaPlus } from 'react-icons/fa6'
+import { useDispatch } from 'react-redux'
+import { openPopup } from '../../Slices/popupSlice'
+import styles from './AddItemButton.module.css'
+
+export default function AddItemButton() {
+    const dispatch = useDispatch()
+
+    return (
+        <button
+            className={styles.addItemButton}
+            onClick={() => dispatch(openPopup('additem'))}
+        >
+            <FaPlus className={styles.addIcon} />
+            ADD ITEM
+        </button>
+    )
+}
