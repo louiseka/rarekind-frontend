a74a3e3b4ac83bf165ca621d6adfacd9938d4c01
Matt Nightingale <matt.snail@gmail.com>
2025-11-23 19:47:18 +0000

    added function to randomly get animal image url. added thumbnail to collection card

--- src/Components/CollectionCard/CollectionCard.jsx
+++ src/Components/CollectionCard/CollectionCard.jsx
@@ -6,11 +6,20 @@ export default function CollectionCard({ collection }) {
     const tags = Array.from(
         new Set(collection.animals.map((animal) => animal.classification_name))
     )
+    const getRandomImage = () => {
+        const animalsWithImages = collection.animals.filter(animal => animal.image_url)
+        const randomIndex = Math.floor(Math.random() * collection.animals.length)
+        if (animalsWithImages.length === 0) return null
+        return collection.animals[randomIndex].image_url
+    }
+    
+    const coverImage = getRandomImage()
+    
     return (
         <Link to={`/collection/${collection.id}`} className={styles.card}>
+            <div>
             <h3 className={styles.cardHeader}>{collection.name}</h3>
-
-            {tags &&
+            {tags && 
                 tags.map((tag) => (
                     <p
                         key={tag}
@@ -32,6 +41,8 @@ export default function CollectionCard({ collection }) {
                 <span className={styles.cardStatusTitle}>Last updated:</span>{' '}
                 <time> {new Date(collection.updated_at).toLocaleString()}</time>
             </p>
+            </div>
+            <img src={coverImage} alt={collection.name} className={styles.coverImage} />
         </Link>
     )
 }
