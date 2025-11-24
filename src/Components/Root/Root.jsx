323067bec79916d7a7535f696ed4f471c53a1566
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-24 15:29:37 +0000

    adding breadcrumbs

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -4,12 +4,15 @@ import Footer from '../Footer/Footer'
 import SiteNav from '../SiteNav/SiteNav'
 import MainContent from '../MainContent/MainContent'
 import Toast from '../Toast/Toast'
+import BreadCrumbs from '../BreadCrumbs/BreadCrumbs'
+import { Outlet } from 'react-router'
 
 function Root() {
     return (
         <div className={styles.wrapper}>
             <Header />
             <SiteNav />
+            <BreadCrumbs />
             <MainContent />
             <Footer />
             <Toast />
